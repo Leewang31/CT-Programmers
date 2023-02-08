cf5e2aaf0daf9a9cf65ccf96e8668c8eedf05614
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-02-09 00:46:35 +0900

    게임 맵 최단거리/bfs/success

--- DFS_BFS/게임맵최단거리.js
+++ DFS_BFS/게임맵최단거리.js
@@ -1,4 +1,4 @@
-//게임 맵 최단거리
+//게임 맵 최단거리/bfs/success
 // 최단거리 dfs/bfs!
 /*
 1. 경기장을 넘어가는 경우와 벽으로 이동하느 경우는 막는다
@@ -6,30 +6,34 @@
 3. 각 타일로 도착한 경우의 최솟 값은 기억합니다.
  */
 function solution(maps) {
-    const mapCount = Array.from(Array(maps.length), () => new Array(maps[0].length).fill(null))
-    const moveFunc =(row,col,point) =>{
-        if(maps[col][row]===1){
-
+    let answer = 1;
+    let visited = maps;
+    let queue = [];
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+    const n = maps.length;
+    const m = maps[0].length;
+    queue.push([0,0])
+    visited[0][0]=0;
+    while(queue.length>0){
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let [x,y] = queue.shift()
+            for (let j = 0; j <4; j++) {
+                let nx = x+dx[j];
+                let ny = y+dy[j];
+                if(nx>=0&&ny>=0&&nx<n&&ny<m && visited[nx][ny]===1){
+                    if(nx === n-1 && ny === m-1){
+                        return answer+1
+                    }
+                    queue.push([nx,ny])
+                    visited[nx][ny]=0
+                }
+            }
         }
-        if(mapCount[col][row] === null || mapCount[col][row]>point) {
-            mapCount[col][row] = point
-            if (row > 0) {
-                moveFunc(row - 1, col, mapCount[col][row])
-            }
-            if (row + 1 < maps[0].length) {
-                moveFunc(row + 1, col, mapCount[col][row])
-            }
-            if (col > 0 && col < map[0].length) {
-                moveFunc(row, col - 1, mapCount[col][row])
-            }
-            if (col + 1 < map[0].length) {
-                moveFunc(row, col + 1, mapCount[col][row])
-            }
-        }
+        answer++
     }
-    moveFunc(0,0,0)
-
-    return mapCount[maps.length-1][maps[0].length-1]
+    return -1
 }
 
 console.log(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
